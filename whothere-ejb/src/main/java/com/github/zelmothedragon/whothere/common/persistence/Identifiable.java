35283c9841e3f760214df9462654dec7827d660a
Maxime MOSELLE <maximemoselle@gmail.com>
2020-10-09 21:11:30 +0200

    Ajout Javadoc

--- whothere-ejb/src/main/java/com/github/zelmothedragon/whothere/common/persistence/Identifiable.java
+++ whothere-ejb/src/main/java/com/github/zelmothedragon/whothere/common/persistence/Identifiable.java
@@ -1,8 +1,26 @@
 package com.github.zelmothedragon.whothere.common.persistence;
 
+/**
+ * Interface générique pour toutes les entitiés identifiables par un identifiant
+ * unique.
+ *
+ * @author MOSELLE Maxime
+ * @param <K> Type de l'identifiant unique
+ */
 public interface Identifiable<K> {
 
+    /**
+     * Accesseur de l'identifiant unique.
+     *
+     * @return L'identifiant unique
+     */
     K getId();
 
+    /**
+     * Mutateur de l'identifiant unique. Cette méthode est présente pour
+     * respecter la convention <i>Java Bean</i>, ne pas appeler explicitement.
+     *
+     * @param id Identifiant unique
+     */
     void setId(K id);
 }
